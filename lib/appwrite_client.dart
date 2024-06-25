9a43ae5fd4e37a58959be343d74b2b4be38410d3
Dimas Prasetio <dimas.prasetio@neuronworks.co.id>
2024-06-26 00:25:46 +0700

    update

--- lib/appwrite_client.dart
+++ lib/appwrite_client.dart
@@ -7,6 +7,6 @@ class AppwriteClientService {
     client
         .setEndpoint('https://cloud.appwrite.io/v1')
         .setProject('6667d453002486a2201f')
-        .setSelfSigned(status: true); // Hanya untuk pengembangan
+        .setSelfSigned(status: true);
   }
 }
